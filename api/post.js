0d42042ce7089431ce4734ee3c8eafbd34bdf62c
Hazel Wu <tinazx056@gmail.com>
2022-05-08 21:00:59 +0800

    feat: 調整 API 格式

--- api/post.js
+++ api/post.js
@@ -3,18 +3,17 @@ import { resHandle } from '../utils/resHandle'
 
 // 取得清單
 export async function getPostListAPI(data) {
-  const resData = await request({
+  return await request({
     url: 'api/posts',
     method: 'get',
     data
   })
 
-  return resHandle(resData)
 }
 
 // 新增貼文
 export async function createPostAPI(data) {
-  const resData = await request({
+  return await request({
     url: 'api/posts',
     method: 'post',
     headers: {
@@ -23,38 +22,34 @@ export async function createPostAPI(data) {
     data
   })
 
-  return resHandle(resData)
 }
 
 // 刪除所有貼文
 export async function deleteAllPostAPI(data) {
-  const resData = await request({
+  return await request({
     url: 'api/posts',
     method: 'delete',
     data
   })
 
-  return resHandle(resData)
 }
 
 // 更新貼文
 export function updatePostAPI(id, data) {
-  const resData = request({
+  return request({
     url: `api/posts/${id}`,
     method: 'patch',
     data
   })
 
-  return resHandle(resData)
 }
 
 // 刪除貼文
 export function deletePostAPI(id) {
-  const resData = request({
+  return request({
     url: `api/posts/${id}`,
     method: 'delete',
   })
 
-  return resHandle(resData)
 }
 
