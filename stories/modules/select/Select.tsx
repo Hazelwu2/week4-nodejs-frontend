eb56f7b5c34949b2dec69a8fab7d53bdfbe2c656
Hazel Wu <tinazx056@gmail.com>
2022-05-14 11:48:58 +0800

    feat: change selectc asc, desc

--- stories/modules/select/Select.tsx
+++ stories/modules/select/Select.tsx
@@ -18,8 +18,8 @@ export const Select = ({ className, defaultValue, onChange }: SelectProps) => {
         defaultValue={defaultValue}
         onChange={onChange}
         className="w-full md:w-[156px] py-2.5 px-4 border-2 border-solid border-dark appearance-none">
-        <option value="desc">最新貼文</option>
-        <option value="asc">舊到新貼文</option>
+        <option value="asc">最新貼文</option>
+        <option value="desc">舊到新貼文</option>
       </select>
       <DownOutlined className="absolute top-1/3 right-3" />
     </div>
